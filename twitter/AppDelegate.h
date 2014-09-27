e580ef8bf3c12b545e4d9c741ca9933d5a215427
taomin <itiachang@gmail.com>
2014-09-27 13:50:23 -0700

    Initial Commit

---
+++ twitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  twitter
+//
+//  Created by Taomin Chang on 9/27/14.
+//  Copyright (c) 2014 Taomin Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ twitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  twitter
+//
+//  Created by Taomin Chang on 9/27/14.
+//  Copyright (c) 2014 Taomin Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
